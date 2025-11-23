14b8806ffda2a26f931f1c511f784e8519d3f970
Elena Manukyan <utv.header@gmail.com>
2025-11-23 14:04:02 +0300

    feat: Reading and parsing files

---
+++ src/Parser.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Hexlet\Code;
+
+class Parser
+{
+    public static function parse(string $filePath)
+    {
+
+        if (!file_exists($filePath)) {
+            throw new \Exception("File '$filePath' does not exist.");
+        }
+
+        $fileContent = file_get_contents($filePath);
+        
+        if ($fileContent === false) {
+            throw new \Exception("Cannot read file '$filePath'.");
+        }
+
+        $data = json_decode($fileContent);
+        
+        if (json_last_error() !== JSON_ERROR_NONE) {
+            $errorMessage = json_last_error_msg();
+            throw new \Exception("Invalid JSON in file '$filePath': $errorMessage");
+        }
+
+        return $data;
+    }
+}
