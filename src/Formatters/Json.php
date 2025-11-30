3ca552962d7af3111cad957fcd5bbd84dab02e6e
Манукьян Елена Михайловна <e.manukyan@aitiguru.com>
2025-11-30 16:10:15 +0300

    fix: correct code by comments of hexlet test

--- src/Formatters/Json.php
+++ src/Formatters/Json.php
@@ -2,7 +2,13 @@
 
 namespace Differ\Formatters\Json;
 
-function format(array $diff): string|false
+function format(array $diff): string
 {
-    return json_encode($diff, JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE);
+    $result = json_encode($diff, JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE);
+
+    if ($result === false) {
+        throw new \Exception("Failed to encode diff to JSON: " . json_last_error_msg());
+    }
+
+    return $result;
 }
